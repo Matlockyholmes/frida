2e134162bc53ef403240c0104c846bbbc8358ac1
Matlockyholmes <matthiasheylen1@gmail.com>
2019-05-23 22:52:26 +0200

    Thuis

--- src/main/java/be/vdab/frida/controllers/SnackController.java
+++ src/main/java/be/vdab/frida/controllers/SnackController.java
@@ -1,7 +1,9 @@
 package be.vdab.frida.controllers;
 
+import be.vdab.frida.services.SnackService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,8 +11,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("snacks")
 public class SnackController {
     private char[] alfabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private final SnackService snackService;
+
+    public SnackController(SnackService snackService) {
+        this.snackService = snackService;
+    }
     @GetMapping
     public ModelAndView toonPagina(){
         return new ModelAndView("snacks","alfabet",alfabet);
+    }
+    @GetMapping("{letter}")
+    public ModelAndView toonSnack(@PathVariable char letter){
+        return new ModelAndView("snacks","snacks", snackService.findByBeginNaam(String.valueOf(letter))).addObject("alfabet",alfabet);
     }
 }
--- src/main/java/be/vdab/frida/services/DefaultSnackService.java
+++ src/main/java/be/vdab/frida/services/DefaultSnackService.java
@@ -2,22 +2,29 @@ package be.vdab.frida.services;
 
 import be.vdab.frida.domain.Snack;
 import be.vdab.frida.repositories.SnackRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
 public class DefaultSnackService implements SnackService{
-    @Autowired
     private SnackRepository snackRepository;
+
+    public DefaultSnackService(SnackRepository snackRepository) {
+        this.snackRepository = snackRepository;
+    }
+
     @Override
     public Optional<Snack> read(long id) {
         return snackRepository.findById(id);
     }
 
     @Override
+    @Transactional(readOnly = false, isolation = Isolation.READ_COMMITTED)
     public void update(Snack snack) {
         snackRepository.update(snack);
     }
