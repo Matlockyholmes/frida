4b761a7630fb88c07b27939d09b619f87f2a334b
MHh10 <matthias.heylen@hi10.be>
2019-05-27 11:27:41 +0200

    "Pour la maison"

--- src/main/java/be/vdab/frida/controllers/SnackController.java
+++ src/main/java/be/vdab/frida/controllers/SnackController.java
@@ -1,15 +1,21 @@
 package be.vdab.frida.controllers;
 
+import be.vdab.frida.domain.Snack;
+import be.vdab.frida.exceptions.SnackNietGevondenException;
 import be.vdab.frida.forms.BeginNaamForm;
+import be.vdab.frida.services.DefaultSnackService;
 import be.vdab.frida.services.SnackService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("snacks")
@@ -42,4 +48,27 @@ public class SnackController {
         }
         return modelAndView.addObject("snacks",snackService.findByBeginNaam(beginNaamForm.getBeginNaam()));
     }
+
+    @GetMapping("wijzigen/form/{id}")
+    public ModelAndView snackWijzigenForm(@PathVariable long id){
+        Snack chosenSnack = snackService.read(id).filter(snack -> snack.getId() == id).get();
+        return new ModelAndView("wijzigen").addObject(chosenSnack);
+    }
+
+    @PostMapping
+    public String snackWijzigen(@Valid Snack snack, Errors errors, RedirectAttributes redirect){
+        if(errors.hasErrors()){
+            return "wijzigen";
+        }
+        try{
+            snackService.update(snack);
+            redirect.addAttribute("gewijzigd",snack.getNaam());
+            return "redirect:/snacks";
+        } catch (SnackNietGevondenException ex){
+            return "redirect:/snacks";
+        }
+
+
+    }
+
 }
